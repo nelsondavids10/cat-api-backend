d4873a46c64d27f193bb8e82b11d782e73c31a72
Nelson David Santacruz <nelson.santacruz@macroproyectos.com>
2024-10-09 15:20:22 -0500

    Ajustes finales proyecto backend

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import routes from './routes';
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.use(express.urlencoded({ extended: true }));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ router.get('/breeds', CatsController.getBreeds);
 router.get('/breeds/:breed_id', CatsController.getBreedById);
 router.get('/breeds/search/:q', CatsController.searchBreeds);
 router.get('/imagesbybreedid', ImagesController.getImagesByBreedId);
-router.get('/login', UsersController.login);
+router.post('/login', UsersController.login);
 router.post('/register', UsersController.register);
 
 export default router;
